7e07f15404c579fe081b8c9a1f53514f87028914
Apple <apple@Apples-MacBook-Pro.local>
2022-09-17 17:59:01 +0300

    for check

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(express.static('dist'))
+
+app.listen(PORT, () => {
+    console.log('Server has been started');
+});
