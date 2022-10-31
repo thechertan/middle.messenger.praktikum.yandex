89bea0b786cf92764e2ef40d98d121dc3fd11c50
Иван <thechertan@yandex.ru>
2022-10-31 22:44:00 +0300

    fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(express.static('dist'));
 
 app.use('*', (_, res) => {
   res.sendFile(__dirname + '/dist/index.html');
